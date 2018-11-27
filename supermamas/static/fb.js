6824fad3e90d1c7c2b029522721300579df9780e
Olli Salonen <oasalonen@gmail.com>
2018-11-27 21:33:38 +0100

    Facebook login

--- supermamas/static/fb.js
+++ supermamas/static/fb.js
@@ -1,19 +1,58 @@
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1946023972118261',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v3.2'
-    });
-      
-    FB.AppEvents.logPageView();   
-      
-  };
+function statusChangeCallback(response) {
+  console.log('statusChangeCallback');
+  console.log(response);
+  // The response object is returned with a status field that lets the
+  // app know the current login status of the person.
+  // Full docs on the response object can be found in the documentation
+  // for FB.getLoginStatus().
+  if (response.status === 'connected') {
+    // Logged into your app and Facebook.
+    //testAPI();
+  } else {
+    // The person is not logged into your app or we are unable to tell.
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into this app.';
+  }
+}
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+// This function is called when someone finishes with the Login
+// Button.  See the onlogin handler attached to it in the sample
+// code below.
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+function fbLogout() {
+  FB.logout(function(response) {
+    // Person is now logged out
+    console.log('Logged out');
+    console.log(response);
+    window.location.reload();
+    return false;
+  });
+}
+  
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '1946023972118261',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v3.2'
+  });
+    
+  FB.AppEvents.logPageView();   
+  
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
