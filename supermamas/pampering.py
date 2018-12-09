73c2a91a5e8b60e5f635fd95ea054ff2fb1b81d5
Olli Salonen <oasalonen@gmail.com>
2018-12-09 16:01:14 +0100

    Initial pampering plan calendar

--- supermamas/pampering.py
+++ supermamas/pampering.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, redirect, request, flash
 from flask_babel import gettext
+from dateutil import rrule
+from datetime import datetime
+
 from supermamas import pamperings
 
 bp = Blueprint("pampering", __name__)
@@ -7,12 +10,20 @@ bp = Blueprint("pampering", __name__)
 @bp.route("/create_pampering", methods=("GET", "POST"))
 def register():
     errors = {}
+    bubble_mama_id = request.args.get("bubble_mama_id")
+    start_date = None
+    end_date = None
 
     if request.method == "POST":
-        user, errors = pamperings.Service().create_pampering(request.form)
-        if not errors:
-            return redirect("/")
+        if request.form["refresh_dates"]:
+            start_date = datetime.strptime(request.form["start_date"], "%Y-%m-%d").date()
+            end_date = datetime.strptime(request.form["end_date"], "%Y-%m-%d").date()
         else:
-            flash(gettext(u"Fix all errors"))
+            user, errors = pamperings.Service().create_pampering(bubble_mama_id, request.form.getlist("date_range[]"))
+            if not errors:
+                return redirect("/")
+            else:
+                flash(gettext(u"Fix all errors"))
 
-    return render_template("create_pampering.html.j2", form_errors=errors, form_values=request.form)
+    pampering_plan = pamperings.Service().prepare_pampering(bubble_mama_id, start_date, end_date)
+    return render_template("create_pampering.html.j2", form_errors=errors, form_values=pampering_plan)
--- supermamas/pamperings/service.py
+++ supermamas/pamperings/service.py
@@ -1,4 +1,7 @@
 from flask_babel import gettext
+from dateutil import rrule
+from datetime import datetime, date, timedelta
+
 from supermamas.pamperings import Signup
 from supermamas.pamperings import Pampering
 from supermamas.accounts import Repository as AccountsRepository
@@ -19,7 +22,27 @@ class Service:
     def _accounts_repository(self):
         return Service.__instance.accounts_repository
 
-    def create_pampering(self, bubble_mama_id, available_dates):
+    def prepare_pampering(self, bubble_mama_id, start_date=None, end_date=None):
+        start_date = start_date if start_date else date.today()
+        end_date = end_date if end_date else start_date + timedelta(days=14)
+        date_range = {}
+        for dt in rrule.rrule(rrule.DAILY, dtstart=datetime(start_date.year, start_date.month, start_date.day), until=datetime(end_date.year, end_date.month, end_date.day)):
+            d = dt.date()
+            date_range[d.isoformat()] = {
+                "selected": True,
+                "label": "{} {}/{}".format(self.weekdays()[d.weekday()], d.day, d.month)
+            }
+
+        return {
+            "bubble_mama_id": bubble_mama_id,
+            "start_date": start_date.isoformat(),
+            "end_date": end_date.isoformat(),
+            "date_range[]": date_range
+            }
+
+    def create_pampering(self, bubble_mama_id, form):
+        available_dates = list(filter(lambda d: d.selected, form.date_range))
+
         pampering = Pampering()
         pampering.bubble_mama = self._accounts_repository().get(bubble_mama_id)
         pampering.available_dates = available_dates
@@ -34,3 +57,6 @@ class Service:
         signup.max_visits = max_visits
         
         self._repository().update_field(pampering_id, "signups." + helping_mama_id, signup)
+
+    def weekdays(self):
+        return [gettext(u"Mon"), gettext(u"Tue"), gettext(u"Wed"), gettext(u"Thu"), gettext(u"Fri"), gettext(u"Sat"), gettext(u"Sun")]
